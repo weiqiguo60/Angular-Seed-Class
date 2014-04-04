8a4f8d3be7bd87a0b38566c0c2c21ceec56fce86
weiqiguo60 <wguo@commandalkon.com>
2014-04-04 13:44:28 -0500

    delete fiel b.txt

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,20 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ['$scope', function($scope) {
+    $scope.car = {make: 'Honda',
+                  model: 'Civic',
+                  year: 2014,
+                  price: 24000
+                 }
 
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MyCtrl2', ['$scope', function($scope) {
+     $scope.cars = [{make: 'Honda', model: 'Civic', year: 2009, price: 19000},
+	                {make: 'Honda', model: 'Civic', year: 2010, price: 20000},
+	                {make: 'Honda', model: 'Civic', year: 2011, price: 21000},
+				    {make: 'Honda', model: 'Civic', year: 2012, price: 22000},
+				    {make: 'Honda', model: 'Civic', year: 2013, price: 23000},
+				    {make: 'Honda', model: 'Civic', year: 2014, price: 24000}]
+    
   }]);
