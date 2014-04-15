26441563679706cd92df08e769a1108f5c9a7a55
weiqiguo60 <wguo@commandalkon.com>
2014-04-15 08:23:30 -0500

    week5

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -3,7 +3,7 @@
 /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
 describe('my app', function() {
-
+  
   browser.get('index.html');
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
@@ -12,17 +12,69 @@ describe('my app', function() {
 
 
   describe('view1', function() {
-
+    var htmlElement;
+	
     beforeEach(function() {
       browser.get('index.html#/view1');
     });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    
+	it('should render view1 when user navigates to /view1', function() {
+      expect(element.all(by.css('[ng-view] form')).first().getAttribute('id')).
+        toMatch(/frmEdit/);
+    });
+    it('should have a "Make:" label', function() {
+		htmlElement = element(by.id('lblMake'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getText()).toEqual('Make:');
+    });
+	it('should have a "Make:" edit', function() {
+		htmlElement = element(by.id('edMake'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getAttribute('value')).toEqual('Honda');
     });
 
+    it('should have a "Make" span', function() {
+	
+    });
+	it('should be able to enter data in the Make edit, edit and span should have the entered value', function() {	
+    });
+    it('should have a "Model:" label', function() {
+	
+    });
+	it('should have a "Model" edit', function() {
+	
+    });
+	it('should have a "Model" span', function() {
+	
+    });
+	it('should be able to enter data in the Model edit, edit and span should have the entered value', function() {
+	
+    });
+	it('should have a "Year:" label', function() {
+	
+    });
+	it('should have a "Year" edit', function() {
+	
+    });
+	it('should have a "Year" span', function() {
+	
+    });
+	it('should be able to enter data in the Year edit, edit and span should have the entered value', function() {
+	
+    });
+	it('should have a "Price:" label', function() {
+	
+    });
+	it('should have a "Price" edit', function() {
+	
+    });
+	it('should have a "Price" span', function() {
+	
+    });
+	it('should be able to enter data in the Price edit, edit and span should have the entered value', function() {
+	
+    });
+    	
   });
 
 
